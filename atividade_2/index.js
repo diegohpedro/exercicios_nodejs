bfaff639add49787c69f4ee58fd49a7330722db2
diegohpedro <diegohpedro@gmail.com>
2020-06-19 20:55:51 -0300

    Resolução do exercicio 2

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,18 @@
 // ## Importe a biblioteca ##
+let rs = require('readline-sync')
+
 
 
 
 // ## Faça o código ##
+let mascara = rs.question(console.log('Você está usando mascara para sair de casa (S ou N) \n'))
+
+let lavarMao = rs.question(console.log('Você está lavando as mãos sempre que possível? (S ou N) \n'))
+
+console.log('=========================================================================')
+
+if (mascara == 'S' && lavarMao == 'S')
+console.log('Párabens você está seguindo o protocolo de segurança')
+
+else ( mascara == 'N' && lavarMao == 'N')
+console.log('Não seja um vacilão, coloque mascara e lave a mão')
