283d06bf81805b3a903ae9b8c470b2a95bb26309
diegohpedro <diegohpedro@gmail.com>
2020-06-19 21:20:51 -0300

    Resolução do exercicio 3

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,9 @@
 // ## Importe a biblioteca ##
-
+let rs = require('readline-sync')
 
 
 // ## Faça o código ##
+let tamanho = rs.question('Qual o tamanho dos lados do quadrado? \n')
+let area =  tamanho * tamanho
+
+console.log ('O área do seu quadrado é ' + area + 'cm')
