f1f001446955a721f31dfb91e855507cd9534d1d
diegohpedro <diegohpedro@gmail.com>
2020-06-20 22:43:14 -0300

    Resolução da atividade 5

--- atividade_5/index.js
+++ atividade_5/index.js
@@ -1,5 +1,9 @@
 // ## Importe a biblioteca ##
-
+let rs = require('readline-sync')
 
 
 // ## Faça o código ##
+let nome = rs.question(console.log('Olá, qual é o seu nome?\n'))
+let texto = rs.question('Digite um texto \n')
+console.log(nome + ' ' + 'Seu texto possui' + ' ' + texto.length + ' ' + 'caracteres')
+ 
